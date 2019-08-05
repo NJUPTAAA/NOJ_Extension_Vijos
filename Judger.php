26d0a394b92eb645f8fe0db879a23452edf4a7a4
David Diao <daviddiaoyf@gmail.com>
2019-08-05 13:24:17 +0800

    updated to support NOJ 0.3.1

--- Judger.php
+++ Judger.php
@@ -2,7 +2,7 @@
 namespace App\Babel\Extension\vijos;
 
 use App\Babel\Submit\Curl;
-use App\Models\SubmissionModel;
+use App\Models\Submission\SubmissionModel;
 use App\Models\ContestModel;
 use App\Models\JudgerModel;
 use Requests;
